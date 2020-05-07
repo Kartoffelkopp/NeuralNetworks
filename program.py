4f95f1823a813dfc76b1548866dab798b77d426a
Kartoffelkopp <mr.kitopa@gmail.com>
2020-05-07 14:56:53 +0200

    inital commit

--- program.py
+++ program.py
@@ -1,6 +1,12 @@
 import neuralnetwork as nn
 import numpy as np
 
+with np.load("mnist.npz") as data:
+    training_images = data["training_images"]
+    training_labels = data["training_labels"]
+
+print(training_labels[0])
+
 layer_sizes = (3,5,10)
 x = np.ones((layer_sizes[0],1))
 
