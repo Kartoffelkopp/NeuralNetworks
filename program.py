bc2b34788014f45fcf6a1a1d8b59e7db7c26f5a0
Kartoffelkopp <mr.kitopa@gmail.com>
2020-05-07 15:15:09 +0200

    inital commit

--- program.py
+++ program.py
@@ -1,16 +1,20 @@
 import neuralnetwork as nn
 import numpy as np
+#import matplotlib.pyplot as plt
 
 with np.load("mnist.npz") as data:
     training_images = data["training_images"]
     training_labels = data["training_labels"]
 
-print(training_labels[0])
+# plt.imshow(training_images[0].reshape(28,28), cmap="gray")
+# plt.show()
+# print(training_labels[0])
 
-layer_sizes = (3,5,10)
-x = np.ones((layer_sizes[0],1))
+layer_sizes = (784,5,10)
+
 
 net = nn.NeuralNetwork(layer_sizes)
-prediction = net.predict(x)
+net.print_accuracy(training_images,training_labels)
+#prediction = net.predict(training_images)
 
-print(prediction)
+#print(np.argmax(prediction[0]))
